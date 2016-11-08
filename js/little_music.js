1b4a34469964a0d3501adbca004252e41a128b67
mhrastnik <mhrastnik@riteh.hr>
2016-11-08 15:19:19 +0100

    Changed a bunch of stuff...

--- js/little_music.js
+++ js/little_music.js
@@ -3,26 +3,13 @@ function stop_audio()
     pause();
 }
 
-function init_audio(song_id)
+var activatedInstruments;
+var audioContext = new AudioContext();
+var songBuffers = {};
+var allInstruments = ['melody', 'piano', 'guitar', 'drums', 'saxophone', 'tambourine'];
+
+function init_audio(song_name)
 {
-    var song_name;
-
-    switch (song_id)
-    {
-        case 'TWINKLE':
-            song_name = 'star';
-            break;
-        case 'FARM':
-            song_name = 'old_macdonald';
-            break;
-        case 'BELLS': 
-            song_name = 'frerejacques';
-            break;
-        case 'SUN': 
-            song_name = 'sunshine';
-            break;
-    }
-
     // Get file locations
     var file_extension = '.ogg'
     var audio_path = {
@@ -38,6 +25,10 @@ function init_audio(song_id)
     // Create audio context
     var audio_ctx = new (window.AudioContext || window.webkitAudioContext)();
 
+
+    var mixer = audio_ctx.createChannelMerger(6);
+    var mixerChannel = 0;
+
     var song_dir = 'res/raw/';
     audio_parts = {};
     for (var key in audio_path) 
@@ -47,76 +38,38 @@ function init_audio(song_id)
             // Create audio objects
             audio_parts[key] = new Audio(song_dir + audio_path[key]);
             audio_parts[key].loop = true;
-            audio_parts[key].preload = true;
+            //audio_parts[key].preload = true;
 
             // Create media element source from object and connect to speaker
             var source = audio_ctx.createMediaElementSource(audio_parts[key]);
-            source.connect(audio_ctx.destination);
+            source.connect(mixer, 0, mixerChannel);
+            mixerChannel++;
 
             // Mute all parts
             audio_parts[key].volume = 0.0;
         }
     }
 
+
+    mixer.connect(audio_ctx.destination);
+
     audio_parts.melody.volume = 1.0;
-}
-
-// Please don't judge me
-function sync_audio() 
-{
-    var targetTime = audio_parts.melody.currentTime;
-
-    audio_parts.piano.currentTime = targetTime;
-    audio_parts.guitar.currentTime = targetTime;
-    audio_parts.drums.currentTime = targetTime;
-    audio_parts.saxophone.currentTime = targetTime;
-    audio_parts.tambourine.currentTime = targetTime;
-}
-
-function play()
-{
-    for (var key in audio_parts) 
-    {
-        if (audio_parts.hasOwnProperty(key)) 
-        {
-            audio_parts[key].play();
-        }
-    } 
-}
-
-function pause() 
-{
-    for (var key in audio_parts) 
-    {
-        if (audio_parts.hasOwnProperty(key)) 
-        {
-            audio_parts[key].pause();
-        }
-    }
 }
 
 function mute_source(source_id) 
 {
-    if (audio_parts.hasOwnProperty(source_id))
+    var indexOfSource = activatedInstruments.indexOf(source_id);
+    if (indexOfSource != -1)
     {
-        audio_parts[source_id].volume = 0;
+        activatedInstruments.splice(indexOfSource, 1);
     }
 }
 
 function unmute_source(source_id) 
 {
-    if (audio_parts.hasOwnProperty(source_id))
-    {
-        audio_parts[source_id].volume = 1;
-    }
-}
-
-function toggle_source_volume(source_id)
-{
-    if (audio_parts.hasOwnProperty(source_id))
-    {
-        if (audio_parts[source_id].volume == 1) mute_source(source_id);
-        else unmute_source(source_id);
+    var indexOfSource = activatedInstruments.indexOf(source_id);
+    if (indexOfSource == -1) {
+        activatedInstruments.push(source_id);
     }
 }
 
@@ -126,10 +79,10 @@ function main()
 
     document.getElementById('back').onclick = goto_song_choose;
 
-    document.getElementById('song_1').onclick = () => goto_song_play('TWINKLE');
-    document.getElementById('song_2').onclick = () => goto_song_play('FARM');
-    document.getElementById('song_3').onclick = () => goto_song_play('BELLS');
-    document.getElementById('song_4').onclick = () => goto_song_play('SUN');
+    document.getElementById('song_1').onclick = () => goto_song_play('star');
+    document.getElementById('song_2').onclick = () => goto_song_play('old_macdonald');
+    document.getElementById('song_3').onclick = () => goto_song_play('frerejacques');
+    document.getElementById('song_4').onclick = () => goto_song_play('sunshine');
 
 }
 
@@ -137,8 +90,7 @@ function goto_song_choose()
 {
     turn_backgrounds_white();
 
-    pause();
-    delete audio_parts;
+    source.stop(0);
 
     document.getElementById('song_play').classList.add('hidden');
     document.getElementById('song_choose').classList.remove('hidden');    
@@ -151,11 +103,64 @@ function goto_song_play(song_id)
     document.getElementById('song_choose').classList.add('hidden');
     document.getElementById('song_play').classList.remove('hidden');    
 
-    init_audio(song_id);
+    document.getElementById('get_ready').classList.remove('hidden');
 
-    if (sync_interval == null) sync_interval = window.setInterval(sync_audio, 12000);
+    activatedInstruments = ['melody'];
+    songBuffers = {};
+    
+    allInstruments.map(instrument =>
+    {
+        getSongBuffer(song_id, instrument, audioContext)
+        .then(buffer =>
+        {
+            songBuffers[instrument] = buffer;
+        
+            if (Object.keys(songBuffers).length != 6) return;
 
-    play();
+            var composedBuffer = composeSong(songBuffers,
+                activatedInstruments,
+                audioContext);
+            
+            source = audioContext.createBufferSource();
+            source.connect(audioContext.destination);
+            source.buffer = composedBuffer;
+            source.loop = true;
+            source.start(0);
+            document.getElementById('get_ready').classList.add('hidden');
+        });
+    })
+
+    // if (sync_interval == null) sync_interval = window.setInterval(sync_audio, 12000);
+
+    // play();
+}
+
+function composeSong(audioBuffers, activatedInstruments, audioContext, existingBuffer)
+{
+    var arrayLength = audioBuffers.melody.getChannelData(0).length;
+
+    var composedBuffer = audioContext.createBuffer(1, arrayLength, 48000);
+    var composedData = composedBuffer.getChannelData(0);
+    activatedInstruments.map(instrument =>
+    {
+        var channelData = audioBuffers[instrument].getChannelData(0);
+        
+        for (var i = 0;i < arrayLength;i++) 
+        {
+            composedData[i] += channelData[i];
+        }
+    });
+    
+    if (existingBuffer != null)
+    {
+        existingData = existingBuffer.getChannelData(0);
+        for (var i = 0;i < arrayLength;i++)
+        {
+            existingData[i] = composedData[i];
+        }
+    }
+    
+    return composedBuffer;
 }
 
 function turn_backgrounds_white()
@@ -194,6 +199,55 @@ function hook_instrument_to_div(instrument)
         toggle_source_volume(instrument);
         toggle_background_color(div, instrument);
     };
+}
+
+function toggle_source_volume(source_id)
+{
+    if (activatedInstruments.indexOf(source_id) == -1)
+    {
+        unmute_source(source_id);
+    }
+    else 
+    {
+        mute_source(source_id);
+    }
+
+    composeSong(songBuffers, activatedInstruments, audioContext, source.buffer);
+}
+/*
+    songId is one of 
+        {'star', 'old_macdonald', 'frerejacques', 'sunshine'}
+    instrumentId is one of 
+        {'melody', 'drums', 'guitar', 'piano', 'saxophone', 'tambourine'}
+*/
+function getSongBuffer(songId, instrumentId, audioContext)
+{
+    return new Promise(function (accept, reject)
+    {
+        var audioPath = 'res/raw/'
+        var audioExtension = '.ogg';
+        var resourceLocation = audioPath + songId + '_' + instrumentId + audioExtension;
+
+        var request = new XMLHttpRequest();
+        request.open('GET', resourceLocation, true);
+        request.responseType = 'arraybuffer';
+
+        request.onload = () => {
+            if (request.status != 200 && request.status != 206)
+            {
+                console.error('Something went wrong');
+                console.log(request);
+                eee = request;
+                reject();
+            }
+            var audioData = request.response;
+
+            audioContext.decodeAudioData(audioData, (buffer) => {
+                accept(buffer);
+            });
+        }
+        request.send();
+    });
 }
 
 window.onload = main;
